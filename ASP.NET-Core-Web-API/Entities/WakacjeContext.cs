d71a2a578bd297fe33fea605cd4a63b59126b494
szypkiwonsz <kacpersawicki321@gmail.com>
2020-05-07 12:45:50 +0200

    Added new class

---
+++ ASP.NET-Core-Web-API/Entities/WakacjeContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_Web_API.Entities
+{
+    public class WakacjeContext
+    {
+    }
+}
