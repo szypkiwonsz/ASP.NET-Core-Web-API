12224248ee815486012afbcbe8276f9230165b76
szypkiwonsz <kacpersawicki321@gmail.com>
2020-05-07 14:28:20 +0200

    Added new class

---
+++ ASP.NET-Core-Web-API/WakacjeProfil.cs
@@ -0,0 +1,21 @@
+﻿using ASP.NET_Core_Web_API.Entities;
+using ASP.NET_Core_Web_API.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_Web_API
+{
+    public class WakacjeProfile : Profile
+    {
+        public WakacjeProfile()
+        {
+            CreateMap<Wyjazd, WakacjeDetailsDto>()
+                .ForMember(m => m.Kraj, map => map.MapFrom(wyjazd => wyjazd.Miejsce.Kraj))
+                .ForMember(m => m.Region, map => map.MapFrom(wyjazd => wyjazd.Miejsce.Region));
+
+        }
+    }
+}
